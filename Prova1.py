801e7c7d88c76a10ff45e39603c407b44cb5a0d0
Marina Magalhães <146394801+MarinaFMagalhaes@users.noreply.github.com>
2023-09-29 16:40:39 -0300

    Add files via upload
    Arquivo referente à Prova do modulo de Logica da Programação da primeira aula.

---
+++ Prova1.py
@@ -0,0 +1,50 @@
+# 1. Crie um exemplo de cada uma das principais tipagens de variáveis.
+
+# Variavel do tipo string (str) - todo e qualquer texto.
+
+nome = "Namjoon"
+nacionalidade = "Coreana"
+profissão = "Rapper, cantor e compositor"
+
+print(type(nome))
+
+print(type(nacionalidade))
+
+print(type(profissão))
+
+# Variavel do tipo inteiro (int) - números inteiros.
+
+idade = 29
+carreira = 12
+
+print(type(idade))
+
+print(type(carreira))
+
+# Variavel do tipo decimal (float) - números decimais.
+
+altura = 1.81
+peso = 77.7
+
+print(type(altura))
+
+print(type(peso))
+
+# Variavel do tipo lógico (bool) - verdadeiro ou falso.
+
+coreano = True
+japones = False
+
+print(type(coreano))
+
+print(type(japones))
+
+# 2. Crie uma solicitação de dados ao usuário.
+
+informação = input("Informe seu nome: ")
+
+print(informação)
+
+# 3. Imprima a solicitação anterior na tela com uma mensagem personalizada.
+
+print(f"Seu nome é {nome}")
